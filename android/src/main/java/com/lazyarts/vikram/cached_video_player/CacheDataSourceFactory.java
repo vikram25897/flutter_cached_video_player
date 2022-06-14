24e3e3c348a6dc725a70c6ae3adfca3d5c39c6a8
Philipp Bauer <ciriousjoker@gmail.com>
2022-06-15 00:43:28 +0200

    Update Exoplayer dependencies & replace deprecations

--- android/src/main/java/com/lazyarts/vikram/cached_video_player/CacheDataSourceFactory.java
+++ android/src/main/java/com/lazyarts/vikram/cached_video_player/CacheDataSourceFactory.java
@@ -4,9 +4,8 @@ import android.content.Context;
 
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultDataSource;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.FileDataSource;
 import com.google.android.exoplayer2.upstream.cache.CacheDataSink;
 import com.google.android.exoplayer2.upstream.cache.CacheDataSource;
@@ -16,29 +15,33 @@ import java.util.Map;
 
 class CacheDataSourceFactory implements DataSource.Factory {
     private final Context context;
-    private DefaultDataSourceFactory defaultDatasourceFactory;
+    private DefaultDataSource.Factory defaultDatasourceFactory;
     private final long maxFileSize, maxCacheSize;
-    private DefaultHttpDataSourceFactory defaultHttpDataSourceFactory;
+
+    private DefaultHttpDataSource.Factory defaultHttpDataSourceFactory;
+
     CacheDataSourceFactory(Context context, long maxCacheSize, long maxFileSize) {
         super();
         this.context = context;
         this.maxCacheSize = maxCacheSize;
         this.maxFileSize = maxFileSize;
-        defaultHttpDataSourceFactory = new DefaultHttpDataSourceFactory("ExoPlayer",
-                null,
-                DefaultHttpDataSource.DEFAULT_CONNECT_TIMEOUT_MILLIS,
-                DefaultHttpDataSource.DEFAULT_READ_TIMEOUT_MILLIS,
-                true);
 
+        defaultHttpDataSourceFactory = new DefaultHttpDataSource.Factory();
+        defaultHttpDataSourceFactory.setUserAgent("ExoPlayer");
+        defaultHttpDataSourceFactory.setAllowCrossProtocolRedirects(true);
     }
-    void setHeaders(Map<String, String> httpHeaders){
-        defaultHttpDataSourceFactory.getDefaultRequestProperties().set(httpHeaders);
+
+    void setHeaders(Map<String, String> httpHeaders) {
+        defaultHttpDataSourceFactory.setDefaultRequestProperties(httpHeaders);
     }
+
     @Override
     public DataSource createDataSource() {
         DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter.Builder(context).build();
-        defaultDatasourceFactory = new DefaultDataSourceFactory(this.context,
-                bandwidthMeter, defaultHttpDataSourceFactory);
+
+        defaultDatasourceFactory = new DefaultDataSource.Factory(this.context, defaultHttpDataSourceFactory);
+        defaultDatasourceFactory.setTransferListener(bandwidthMeter);
+
         SimpleCache simpleCache = SimpleCacheSingleton.getInstance(context, maxCacheSize).simpleCache;
         return new CacheDataSource(simpleCache, defaultDatasourceFactory.createDataSource(),
                 new FileDataSource(), new CacheDataSink(simpleCache, maxFileSize),
