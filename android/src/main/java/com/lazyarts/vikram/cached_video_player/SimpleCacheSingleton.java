71c56086d3a0a33a3ddbc781e2da7d3367fcf10a
Philipp Bauer <ciriousjoker@gmail.com>
2022-12-10 16:00:53 +0100

    Update Exoplayer dependencies & replace deprecations (#57)

--- android/src/main/java/com/lazyarts/vikram/cached_video_player/SimpleCacheSingleton.java
+++ android/src/main/java/com/lazyarts/vikram/cached_video_player/SimpleCacheSingleton.java
@@ -2,7 +2,7 @@ package com.lazyarts.vikram.cached_video_player;
 
 import android.content.Context;
 
-import com.google.android.exoplayer2.database.ExoDatabaseProvider;
+import com.google.android.exoplayer2.database.StandaloneDatabaseProvider;
 import com.google.android.exoplayer2.upstream.cache.LeastRecentlyUsedCacheEvictor;
 import com.google.android.exoplayer2.upstream.cache.SimpleCache;
 
@@ -16,7 +16,7 @@ public class SimpleCacheSingleton {
 
     private SimpleCacheSingleton(Context context, long maxCacheSize) {
         evictor = new LeastRecentlyUsedCacheEvictor(maxCacheSize);
-        simpleCache = new SimpleCache(new File(context.getCacheDir(), "media"), evictor, new ExoDatabaseProvider(context));
+        simpleCache = new SimpleCache(new File(context.getCacheDir(), "media"), evictor, new StandaloneDatabaseProvider(context));
     }
 
     public synchronized static SimpleCacheSingleton getInstance(Context context, long maxCacheSize) {
